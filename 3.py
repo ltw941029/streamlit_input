6fedb08fefb2575ac15af1e1587ad868e1984a66
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 16:58:57 +0900

    Create 3.py

---
+++ 3.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.header('이것은 헤더입니다', anchor='https://docs.streamlit.io/library/api-reference/text/st.header' ,help='anchor 존재') 
+
+st.header('이것은 헤입니다', anchor=None ,help='anchor ') 
