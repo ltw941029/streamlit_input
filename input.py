d63a7f68080ed544e6ce976ea3a443617b939c0b
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 16:25:07 +0900

    Update input.py

--- input.py
+++ input.py
@@ -1,9 +1,9 @@
 import streamlit as st
 
 st.markdown(f"""## 안녕하세요
-<font size=16>symbol</font> code
-""", unsafe_allow_html=True)
+<font size=16>환영</font> 해요
+""", unsafe_allow_html=True, help='unsafe_allow_html=True인 경우')
 
 st.markdown(f"""## 안녕하세요
-<font size=16>symbol</font> code
-""", unsafe_allow_html=False)
+<font size=16>환영</font> 해요
+""", unsafe_allow_html=False, help='unsafe_allow_html=False인 경우')
