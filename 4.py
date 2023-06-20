f39dab9f54ce4d1cb6c1e7ce2980e63bb6787fc3
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 17:05:49 +0900

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.subheader('이것은 헤더입니다', anchor='https://docs.streamlit.io/library/api-reference/text/st.subheader' ,help='anchor 존재') 
+
+st.subheader('이것은 헤입니다', anchor=None ,help='anchor ') 
