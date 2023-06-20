716e2f429997ca1169054acfe62bf63cfd775772
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 16:21:24 +0900

    Update input.py

--- input.py
+++ input.py
@@ -1,11 +1,9 @@
 import streamlit as st
 
 st.markdown(f"""## 안녕하세요
-
 <font size=16>symbol</font> code
 """, unsafe_allow_html=True)
 
 st.markdown(f"""## 안녕하세요
-
 <font size=16>symbol</font> code
 """, unsafe_allow_html=False)
