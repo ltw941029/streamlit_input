882cb5936855c163bad18fa04c788001d9ed44ff
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 16:46:07 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-st.title('이것은 제목입니다', anchor=https://docs.streamlit.io/library/api-reference/text/st.title ,help='anchor 존재') 
+st.title('이것은 제목입니다', anchor='https://docs.streamlit.io/library/api-reference/text/st.title' ,help='anchor 존재') 
 
 st.title('이것은 제목입니다', anchor=None ,help='anchor ') 
