0cb682b91a724afabc597637635ae568fc9c69c1
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-06-20 17:11:08 +0900

    Create 5.py

---
+++ 5.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.caption(f"""## 안녕하세요
+<font size=16>환영</font> 해요
+""", unsafe_allow_html=True, help='unsafe_allow_html=True인 경우')
+
+st.caption(f"""## 안녕하세요
+<font size=16>환영</font> 해요
+""", unsafe_allow_html=False, help='unsafe_allow_html=False인 경우')
