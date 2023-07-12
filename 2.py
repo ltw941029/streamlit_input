6314f89509c5780fcd3b3e12a2bb81e7e178e596
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-07-12 12:13:06 +0900

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
-st.title('이것은 제목입니다', anchor='https://docs.streamlit.io/library/api-reference/text/st.title' ,help='anchor 존재') 
-
-st.title('이것은 제목입니다', anchor=None ,help='anchor ') 
+# anchor가 있는 경우
+st.title("이것은 제목입니다", anchor="https://docs.streamlit.io/library/api-reference/text/st.title" , help="anchor 존재") 
+# anchor가 None인 경우
+st.title('이것은 제목입니다', anchor=None)
