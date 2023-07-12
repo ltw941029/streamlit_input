611e161d346388058a192cc435a86eb2dfd43ff6
ltw941029 <89626446+ltw941029@users.noreply.github.com>
2023-07-12 12:02:25 +0900

    Update 4.py

--- 4.py
+++ 4.py
@@ -1,5 +1,5 @@
 import streamlit as st
-
-st.subheader('이것은 헤더입니다', anchor='https://docs.streamlit.io/library/api-reference/text/st.subheader' ,help='anchor 존재') 
-
-st.subheader('이것은 헤입니다', anchor=None ,help='anchor ') 
+#anchor가 있는 경우
+st.subheader("이것은 서브헤더입니다.", anchor="https://docs.streamlit.io/library/api-reference/text/st.subheader" ,help="anchor 존재") 
+# anchor가 None 경우
+st.subheader("이것은 서브헤더입니다.", anchor=None)
